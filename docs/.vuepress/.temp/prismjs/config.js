516d6f0ac0991c77d634511e5f59a22f26fbed96
zhaomengyao <3036190149@qq.com>
2025-07-23 23:43:28 +0800

    first commit

---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,13 @@
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/index.js"
+import "C:/D/github/16621275149/16621275149.github.io/node_modules/@vuepress/highlighter-helper/lib/client/styles/code-block-title.css"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
